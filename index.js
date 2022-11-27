2e99daa6ff6e474419d8d2581f49a2da0a10f186
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-28 00:34:41 +0600

    added database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -8,6 +9,37 @@ const app = express()
 // middleware
 app.use(cors());
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nl9uncn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+async function run() {
+    try{
+        const usersCollection = client.db("mobileMart").collection("users");
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
+
+
+
+
+
+
+
+
+
+
 
 
 
